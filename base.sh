a570f4332cc3161ccb101d4bb031ad7d05b945b1
Arialdo Martini <arialdomartini@gmail.com>
2015-01-02 13:13:02 +0100

    No need to set flags to false

--- base.sh
+++ base.sh
@@ -21,41 +21,38 @@ function build_prompt {
     local prompt=""
     # Git info
     local current_commit_hash=$(git rev-parse HEAD 2> /dev/null)
-    if [[ -n $current_commit_hash ]]; then local is_a_git_repo=true; else local is_a_git_repo=false; fi
+    if [[ -n $current_commit_hash ]]; then local is_a_git_repo=true; fi
     
     if [[ $is_a_git_repo == true ]]; then
         local current_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-        if [[ $current_branch == 'HEAD' ]]; then local detached=true; else local detached=false; fi
+        if [[ $current_branch == 'HEAD' ]]; then local detached=true; fi
 
         local number_of_logs="$(git log --pretty=oneline -n1 2> /dev/null | wc -l)"
         if [[ $number_of_logs -eq 0 ]]; then
             local just_init=true
         else
             local upstream=$(git rev-parse --symbolic-full-name --abbrev-ref @{upstream} 2> /dev/null)
-            if [[ -n "${upstream}" && "${upstream}" != "@{upstream}" ]]; then local has_upstream=true; else local has_upstream=false; fi
+            if [[ -n "${upstream}" && "${upstream}" != "@{upstream}" ]]; then local has_upstream=true; fi
 
             local git_status="$(git status --porcelain 2> /dev/null)"
         
-            if [[ $git_status =~ ($'\n'|^).M ]]; then local has_modifications=true; else local has_modifications=false; fi
+            if [[ $git_status =~ ($'\n'|^).M ]]; then local has_modifications=true; fi
         
-            if [[ $git_status =~ ($'\n'|^)M ]]; then local has_modifications_cached=true; else local has_modifications_cached=false; fi
+            if [[ $git_status =~ ($'\n'|^)M ]]; then local has_modifications_cached=true; fi
         
-            if [[ $git_status =~ ($'\n'|^)A ]]; then local has_adds=true; else local has_adds=false; fi
+            if [[ $git_status =~ ($'\n'|^)A ]]; then local has_adds=true; fi
         
-            if [[ $git_status =~ ($'\n'|^).D ]]; then local has_deletions=true; else local has_deletions=false; fi
+            if [[ $git_status =~ ($'\n'|^).D ]]; then local has_deletions=true; fi
         
-            if [[ $git_status =~ ($'\n'|^)D ]]; then local has_deletions_cached=true; else local has_deletions_cached=false; fi
+            if [[ $git_status =~ ($'\n'|^)D ]]; then local has_deletions_cached=true; fi
         
-            if [[ $git_status =~ ($'\n'|^)[MAD] && ! $git_status =~ ($'\n'|^).[MAD\?] ]]; then local ready_to_commit=true; else local ready_to_commit=false; fi
+            if [[ $git_status =~ ($'\n'|^)[MAD] && ! $git_status =~ ($'\n'|^).[MAD\?] ]]; then local ready_to_commit=true; fi
         
             local number_of_untracked_files=`echo "${git_status}" | $(sh -c 'which grep') -c "^??"`
-            if [[ $number_of_untracked_files -gt 0 ]]; then local has_untracked_files=true; else has_untracked_files=false; fi
+            if [[ $number_of_untracked_files -gt 0 ]]; then local has_untracked_files=true; fi
         
             local tag_at_current_commit=$(git describe --exact-match --tags $current_commit_hash 2> /dev/null)
-            if [[ -n $tag_at_current_commit ]]; then local is_on_a_tag=true; else local is_on_a_tag=false; fi
-        
-            local has_diverged=false
-            local can_fast_forward=false
+            if [[ -n $tag_at_current_commit ]]; then local is_on_a_tag=true; fi
         
             if [[ $has_upstream == true ]]; then
                 local commits_diff="$(git log --pretty=oneline --topo-order --left-right ${current_commit_hash}...${upstream} 2> /dev/null)"
@@ -65,9 +62,6 @@ function build_prompt {
             if [[ $commits_ahead -gt 0 && $commits_behind -gt 0 ]]; then
                 local has_diverged=true
             fi
-            if [[ $commits_ahead -eq 0 && $commits_behind -gt 0 ]]; then
-                local can_fast_forward=true
-            fi
         
             local will_rebase=$(git config --get branch.${current_branch}.rebase 2> /dev/null)
         
@@ -76,7 +70,7 @@ function build_prompt {
             else
                 local number_of_stashes=0
             fi
-            if [[ $number_of_stashes -gt 0 ]]; then local has_stashes=true; else local has_stashes=false; fi
+            if [[ $number_of_stashes -gt 0 ]]; then local has_stashes=true; fi
         fi
     fi
     
