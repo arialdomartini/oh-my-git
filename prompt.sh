7e99e94fa82a15af2f5bf6332c8a7796b61c8131
Erich E. Hoover <ehoover@sweptlaser.com>
2017-08-09 15:14:46 -0600

    Use an 'untracked files' symbol that is also available in Courier New.

--- prompt.sh
+++ prompt.sh
@@ -9,7 +9,7 @@ if [ -n "${BASH_VERSION}" ]; then
 
     : ${omg_is_a_git_repo_symbol:=''}
     : ${omg_submodules_outdated_symbol:=''}
-    : ${omg_has_untracked_files_symbol:=''}        #                ?    
+    : ${omg_has_untracked_files_symbol:=''}        #                ?    
     : ${omg_has_adds_symbol:=''}
     : ${omg_has_deletions_symbol:=''}
     : ${omg_has_cached_deletions_symbol:=''}
