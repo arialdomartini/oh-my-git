30e3bbf410b7ba8b89f15e9bc375750230481585
Christian Kuehn <kuehn@synyx.de>
2017-12-09 16:52:46 +0100

    use unicode arrow for 'has_renames'

--- prompt.sh
+++ prompt.sh
@@ -14,7 +14,7 @@ if [ -n "${BASH_VERSION}" ]; then
     : ${omg_has_adds_symbol:=''}
     : ${omg_has_deletions_symbol:=''}
     : ${omg_has_cached_deletions_symbol:=''}
-    : ${omg_has_renames_symbol:=''}                # 
+    : ${omg_has_renames_symbol:='➔'}                # 
     : ${omg_has_modifications_symbol:=''}
     : ${omg_has_cached_modifications_symbol:=''}
     : ${omg_ready_to_commit_symbol:=''}            #   →
